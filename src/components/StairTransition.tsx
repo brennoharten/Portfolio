d938fb3d120aac8fd23899c7e9b8e7eed98d4846
brennoharten <brennoharten@gmail.com>
2025-01-03 17:56:18 -0300

    adicionando efeito escada

--- src/components/StairTransition.tsx
+++ src/components/StairTransition.tsx
@@ -2,13 +2,17 @@
 
 import { AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
+import Stairs from "./Stairs";
 
 export default function StairTransition() {
     const pathname = usePathname()
     return (
         <AnimatePresence mode="wait">
             <div key={pathname}>
-                <div className="h-screen w-screen fixed top-0 left-0 rigth-0">satirs</div>
+                <div className="h-screen w-screen fixed top-0 left-0 rigth-0 
+                    pointer-events-none z-40 flex">
+                        <Stairs/>
+                </div>
             </div>
         </AnimatePresence>
     )
