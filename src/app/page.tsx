7bfbac727bbc02c071b831ea8c8f14cc690ce067
brennoharten <brennoharten@gmail.com>
2025-01-05 22:50:55 -0300

    astart homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import { Button } from "@/components/ui/button";
+import {FiDownload} from "react-icons/fi"
 
 export default function Home() {
   return (
-    <main className="relative ">
-      <div>
-        <h1> Homepage</h1>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xk:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span>Software Developer</span>
+            <h1>Hello I'm <br/> <span>Brenno Harten</span></h1>
+          </div>
+          <div>photo</div>
+        </div>
       </div>
-    </main>
+    </section>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ import MobileNav from "./MobileNav"
 
 export default function Header() {
     return (
-        <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+        <header className="py-8 xl:py-12 text-white bg-pink-50/10">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/">
                     <h1 className="text-4xl font-semibold">
