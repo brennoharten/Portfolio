293dd6de26a49b781e84be72c28ca6e5babe2f4e
brennoharten <brennoharten@gmail.com>
2025-01-17 14:19:52 -0300

    fineshed services page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,4 +1,47 @@
+"use cliente"
 
+import { FaJs, FaReact, FaNodeJs, FaDocker} from "react-icons/fa"
+import { SiDocker, SiDotnet, SiTailwindcss, SiNextdotjs, SiOpenai } from "react-icons/si"
+import { VscAzureDevops } from "react-icons/vsc";
+
+const about = {
+    title: 'About me',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit sapiente cum numquam nihil temporibus cupiditate, itaque fugit, maxime magnam repellendus esse aspernatur iste corporis',
+    info: [
+        {
+            fieldName: 'Name',
+            fieldValue: 'Brenno Harten'
+        },
+        {
+            fieldName: 'Phone',
+            fieldValue: '(+55) 85 99832 7174'
+        },
+        {
+            fieldName: 'Experience',
+            fieldValue: '+5 years'
+        },
+        {
+            fieldName: 'Email',
+            fieldValue: 'brennoharten@gmail.com'
+        },
+        {
+            fieldName: 'Nationality',
+            fieldValue: 'Brazilian'
+        },
+        {
+            fieldName: 'LinkedIn',
+            fieldValue: 'https://www.linkedin.com/in/brennoflorencio/'
+        },
+        {
+            fieldName: 'Name',
+            fieldValue: 'Brenno Harten'
+        },
+        {
+            fieldName: 'Name',
+            fieldValue: 'Brenno Harten'
+        },
+    ]
+}
 
 export default function page() {
     return (
