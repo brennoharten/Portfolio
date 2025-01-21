df39b906871e47c6fd6f2f3ac1949b874c1b1f60
brennoharten <brennoharten@gmail.com>
2025-01-21 20:48:42 -0300

    adicionando pagina resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,6 +1,7 @@
 "use cliente"
 
-import { FaJs, FaReact, FaNodeJs, FaDocker} from "react-icons/fa"
+import { Description } from "@radix-ui/react-dialog";
+import { FaJs, FaReact, FaNodeJs, FaDocker } from "react-icons/fa"
 import { SiDocker, SiDotnet, SiTailwindcss, SiNextdotjs, SiOpenai } from "react-icons/si"
 import { VscAzureDevops } from "react-icons/vsc";
 
@@ -33,18 +34,89 @@ const about = {
             fieldValue: 'https://www.linkedin.com/in/brennoflorencio/'
         },
         {
-            fieldName: 'Name',
-            fieldValue: 'Brenno Harten'
+            fieldName: 'Freelance',
+            fieldValue: 'Available'
         },
         {
-            fieldName: 'Name',
-            fieldValue: 'Brenno Harten'
+            fieldName: 'Languages',
+            fieldValue: 'Inglish, Portuguese'
         },
     ]
 }
 
+const experience = {
+    icon: "/assets/resume/badge.svg",
+    title: "My experience",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    items: [
+        {
+            company: "AeC - Contact Center",
+            position: "Software developer",
+            duration: "2021 - Present"
+        },
+        {
+            company: "AeC - Contact Center",
+            position: "Software developer",
+            duration: "2021 - Present"
+        }
+    ]
+}
+
+const education = {
+    icon: "/assets/resume/cap.svg",
+    title: "My education",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    items: [
+        {
+            institution: "University of Fortaleza",
+            degree: "Computer Ciensce",
+            conclusion: "2023"
+        },
+        {
+            institution: "University of Fortaleza",
+            degree: "Computer Ciensce",
+            conclusion: "2023"
+        },
+        {
+            institution: "University of Fortaleza",
+            degree: "Computer Ciensce",
+            conclusion: "2023"
+        },
+    ]
+}
+
+const skills = {
+    title: "My skills",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    skillist: [
+        {
+            icon: <FaJs />,
+            name: "Java script"
+        },
+        {
+            icon: <FaJs />,
+            name: "Java script"
+        },
+        {
+            icon: <FaJs />,
+            name: "Java script"
+        },
+        {
+            icon: <FaJs />,
+            name: "Java script"
+        },
+    ]
+}
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { motion } from 'framer-motion'
+
 export default function page() {
     return (
-        <div>resume page</div>
+        <motion.div initial={{opacity: 0}}>
+
+        </motion.div>
     )
 }
